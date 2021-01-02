82a0c1172af2404cc526b95c8b0e5e090512b836
TJ <tjdrzewiecki@gmail.com>
2021-01-02 13:41:59 -0500

    Added mid-chaining display and disabled decimal after 1 use per display

--- calculator.js
+++ calculator.js
@@ -24,18 +24,40 @@ const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
 const display = document.querySelector('#window');
 const numbers = document.querySelector('.numbers').childNodes;
-let display_value = display.textContent;
+const point = document.querySelector('#point');
+
 let operator = '';
+let display_value = '';
+let calc = false;
+let decimal = false;
+
+function decimalCheck (){
+    if (decimal == false) {
+        point.disabled = false;
+    }
+    else{
+        point.disabled = true;
+    }
+}
 
 clear.addEventListener('click', function(){
     display.textContent = '';
     display_value = '';
     operator = '';
+    decimal = false;
 })
 
 numbers.forEach(number => {
-    number.addEventListener('click', function e(){
+    number.addEventListener('click', function(){
+        if (calc == true) {
+            display.textContent = '';
+        }
+        calc = false;
         display.textContent += number.textContent;
+        if (display.textContent.match(/\./) != null){
+            decimal = true;
+        }
+        decimalCheck();
     })
 })
 
@@ -45,7 +67,8 @@ plus.addEventListener('click', function(){
     }
     display_value = display.textContent;
     operator = plus;
-    display.textContent = '';
+    calc = true;
+    decimal = false;
 })
 
 minus.addEventListener('click', function(){
@@ -54,7 +77,8 @@ minus.addEventListener('click', function(){
     }
     display_value = display.textContent;
     operator = minus;
-    display.textContent = '';
+    calc = true;
+    decimal = false;
 })
 
 times.addEventListener('click', function(){
@@ -63,7 +87,8 @@ times.addEventListener('click', function(){
     }
     display_value = display.textContent;
     operator = times;
-    display.textContent = '';
+    calc = true;
+    decimal = false;
 })
 
 divBy.addEventListener('click', function(){
@@ -72,9 +97,12 @@ divBy.addEventListener('click', function(){
     }
     display_value = display.textContent;
     operator = divBy;
-    display.textContent = '';
+    calc = true;
+    decimal = false;
 })
 
 equals.addEventListener('click', function(){
     display.textContent = operate(operator, Number(display_value), Number(display.textContent));
+    operator = '';
+    decimal = false;
 })
