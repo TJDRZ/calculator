3d5573823c18619bcf691dbd51a6a66530be23f5
TJ <tjdrzewiecki@gmail.com>
2021-01-02 08:36:54 -0500

    Added chaining functionality

--- calculator.js
+++ calculator.js
@@ -34,30 +34,42 @@ clear.addEventListener('click', function(){
 })
 
 numbers.forEach(number => {
-    number.addEventListener('click', function(){
+    number.addEventListener('click', function e(){
         display.textContent += number.textContent;
     })
 })
 
 plus.addEventListener('click', function(){
+    if (operator != '') {
+        display.textContent = operate(operator, Number(display_value), Number(display.textContent));
+    }
     display_value = display.textContent;
     operator = plus;
     display.textContent = '';
 })
 
 minus.addEventListener('click', function(){
+    if (operator != '') {
+        display.textContent = operate(operator, Number(display_value), Number(display.textContent));
+    }
     display_value = display.textContent;
     operator = minus;
     display.textContent = '';
 })
 
 times.addEventListener('click', function(){
+    if (operator != '') {
+        display.textContent = operate(operator, Number(display_value), Number(display.textContent));
+    }
     display_value = display.textContent;
     operator = times;
     display.textContent = '';
 })
 
 divBy.addEventListener('click', function(){
+    if (operator != '') {
+        display.textContent = operate(operator, Number(display_value), Number(display.textContent));
+    }
     display_value = display.textContent;
     operator = divBy;
     display.textContent = '';
